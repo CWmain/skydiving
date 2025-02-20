11e6b0480cae91891e77206b6be63a89110275b7
CWmain <cameronwhite2478@gmail.com>
2025-02-20 17:49:06 +1100

    Adjusted logic such that landing on the ground only triggers once

--- objects/skydiver/skydiver.gd
+++ objects/skydiver/skydiver.gd
@@ -8,8 +8,10 @@ const JUMP_VELOCITY = -400.0
 @export var ascendSpeed: int = 100
 @export var GM_HOLDER: SubViewportContainer
 
+var landed: bool = false
 var GM: gameManager
 var fallSpeed = 0
+
 func _ready():
 	GM = GM_HOLDER.GM
 	assert(GM != null)
@@ -20,22 +22,26 @@ func _physics_process(delta):
 		position.y += fallSpeed * delta
 		GM.current_height -= fallSpeed * delta
 		print("Postion: %s\nCurrent Height: %s" % [str(position), str(GM.current_height)] )
-		return
-	elif GM.lockScreen:
+		
+	elif GM.lockScreen and !landed:
 		position.y = 360-GM.groundHeight
+		landed = true
 		print("Now user has hit ground, select correct sprite, wait, then end game displaying speed as a score")
+	
+	# Prevent all movement when screen locked for the end of the game
+	if GM.lockScreen:
 		return
 
 	# Add the gravity.
-	var upDown = Input.get_axis("ui_up", "ui_down")
+	var upDown = Input.get_axis("up", "down")
 	if upDown:
 		position.y += upDown * SPEED*delta
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		position.x += direction * SPEED * delta
+	var leftRight = Input.get_axis("left", "right")
+	if leftRight:
+		position.x += leftRight * SPEED * delta
 	
 	if position.y > 100:
 		position.y += -ascendSpeed*delta
